b6cad9fc9963b15ad869e6d109b415e3fcb70490
yuki0508 <enla.festa.la.vita@icloud.com>
2019-02-13 13:27:47 +0900

    ブログアプリ

---
+++ memo/app/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from .models import Memo
+class MemoForm(ModelForm):
+    class Meta:
+        model=Memo
+        fields=['title','text']
---
+++ memo/app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from .models import Memo
+from django.shortcuts import get_object_or_404
+from .forms import MemoForm
+from django.views.decorators.http import require_POST
+def index(request):
+  memos = Memo.objects.all().order_by('-updated_datetime')
+  return render(request, 'app/index.html', { 'memos': memos })
+
+
+def detail(request, memo_id):
+    #get_object_or_404 モデルの中のmemo_idにないidが
+    #入力された時にエラーではなく404ページを表示する
+  memo = get_object_or_404(Memo, id=memo_id)
+  return render(request, 'app/detail.html', {'memo': memo})
+
+
+
+#データベースに保存するときの典型的な形
+def new_memo(request):
+	if request.method == "POST":
+		form = MemoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('app:index')
+	else:
+		form = MemoForm
+	return render(request, 'app/new_memo.html', {'form': form })
+
+
+
+#method==GETのときは実行されない
+@require_POST
+def delete_memo(request, memo_id):
+	memo = get_object_or_404(Memo, id=memo_id)
+	memo.delete()
+	return redirect('app:index')
+
+#データベースの更新
+def edit_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    if request.method == "POST":
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'app/edit_memo.html', {'form': form, 'memo':memo })
